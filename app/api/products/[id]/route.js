d65f2bea252db80532ee38062885888aff54dc40
Zeineblch3 <lallouzei@gmail.com>
2026-02-21 10:39:33 +0100

    fix branch1

--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../src/lib/prisma";
+
 export async function GET(request, { params }) {
   try {
     const { id } = params;
